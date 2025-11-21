0aecd76892acfc736119a7e49f9bb0251f9a43c2
11249a370-netizen <11249a370@kanchiuniv.ac.in>
2025-11-21 13:09:16 +0530

    Update Q11_treeDFS.c

--- DSA_PROGRAMS/Q11_treeDFS.c
+++ DSA_PROGRAMS/Q11_treeDFS.c
@@ -1,3 +1,6 @@
+AIM:
+To implement a Binary Search Tree (BST)  and perform preorder, inorder, and postorder
+traversals on the tree after inserting elements dynamically.
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -92,3 +95,105 @@ int main()
     postorder(root);
     return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for tree node
+// Data, Pointer to left child and Pointer to right child
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+
+// creating a node
+struct Node *createNode(int value)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert the new node and returns root node of the updated tree
+struct Node *insert(struct Node *root, int value)
+{
+    // First node will be the  root node
+    if (root == NULL)
+    {
+        return createNode(value);
+    }
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Preorder - Root -> Left -> Right
+void preorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Inorder - Left -> Root -> Right
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+// Postorder - Left -> Right -> Root
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main()
+{
+
+    int n, value;
+    struct Node *root = NULL;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d values:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+        root = insert(root, value); // Inserting dynamically into BST
+    }
+
+    printf("Preorder Traversal: ");
+    preorder(root);
+
+    printf("\nInorder Traversal: ");
+    inorder(root);
+
+    printf("\nPostorder Traversal: ");
+    postorder(root);
+    return 0;
+}
+OUTPUT:
+Enter number of elements: 5
+Enter 5 values:
+93 98 44 69 79
+Preorder Traversal: 93 44 69 79 98 
+Inorder Traversal: 44 69 79 93 98 
+Postorder Traversal: 79 69 44 98 93 
+
