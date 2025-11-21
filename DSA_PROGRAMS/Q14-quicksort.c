a56c1349cf5748d84d13430135e4a405f4545a30
11249a370-netizen <11249a370@kanchiuniv.ac.in>
2025-11-21 13:28:06 +0530

    Create Q14-quicksort.c

---
+++ DSA_PROGRAMS/Q14-quicksort.c
@@ -0,0 +1,67 @@
+AIM:
+To write a C program to sort an array of elements using the Quick
+Sort algorithm based on the divide-and-conquer approach.
+ SOURCE CODE:
+#include <stdio.h>
+
+//Swaping the numbers / Call by referene to persist changes
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partition 
+int partition(int list[], int low, int high)
+{
+    int pivot = list[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (list[j] < pivot)
+        {
+            i++;
+            swap(&list[i], &list[j]);
+        }
+    }
+    swap(&list[i + 1], &list[high]);
+    return i + 1;
+}
+
+// Quick Sort function
+void quickSort(int list[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(list, low, high);
+        quickSort(list, low, pi - 1);
+        quickSort(list, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int list[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &list[i]);
+
+    quickSort(list, 0, n - 1);
+
+    printf("Sorted array (Quick Sort): ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", list[i]);
+
+    return 0;
+}
+OUTPUT:
+Enter number of elements: 7
+Enter 7 elements:
+465 64 98 855 917 9 2
+Sorted array (Quick Sort): 2 9 64 98 465 855 917 
